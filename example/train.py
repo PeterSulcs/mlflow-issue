fd12ecd27102def81a1cccc22bf24a5e03e6bb30
Peter Sulcs <Peter.Sulcs@gmail.com>
2021-02-02 09:17:09 -0800

    replicate SSL error

--- example/train.py
+++ example/train.py
@@ -6,6 +6,7 @@ import mlflow
 import mlflow.sklearn
 
 if __name__ == "__main__":
+    mlflow.set_experiment("example")
     X = np.array([-2, -1, 0, 1, 2, 1]).reshape(-1, 1)
     y = np.array([0, 0, 1, 1, 1, 0])
     lr = LogisticRegression()
